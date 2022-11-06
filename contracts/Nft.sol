a35a66417d1d3df3fbf99c237b56af3d5fcf3611
bdtaskrony <bdtask.rony@gmail.com>
2022-11-06 23:39:16 +0600

    first commit

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -5,6 +5,6 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNFT") {
+    constructor() ERC721("NFTName", "MNFT") {
     }
 }
