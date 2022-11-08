d4c8dc9f7881d22ea62bd38ea0d336cd8522cf5c
Md. Mainul Islam Roni <mainul.roni@springrain.io>
2022-11-08 15:07:12 +0600

    update test nft

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -2,9 +2,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721 {
-    constructor() ERC721("NFTName", "MNFT") {
+contract Nft is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
     }
-}
+
+    function createNft(string calldata tokenUri) public returns (uint256) {
+        uint256 currentTokenId = _tokenIds.current();
+        _safeMint(msg.sender, currentTokenId);
+        _setTokenURI(currentTokenId, tokenUri);
+        _tokenIds.increment();
+        return currentTokenId;
+    }
+} 
